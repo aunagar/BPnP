77bdd8af3c1c1de9ba60e47035f2751eca98b614
Ajay Unagar <ajayunagar@gmail.com>
2020-04-20 16:14:28 +0200

    requiremensts without conda

--- requirements.sh
+++ requirements.sh
@@ -3,16 +3,20 @@
 # module load CUDA/9.0.176 GCC/5.4.0-2.26 NCCL/2.3.7-CUDA-9.0.176
 
 ########################### build conda env ###################################
-conda init
-source ~/.bashrc
-conda create -n bpnp python=3.7 -y
+# conda init
+# source ~/.bashrc
+# conda create -n bpnp python=3.7 -y
+python -m venv ~/.bpnp
 
 ########################### activate env ###################################
-eval "$(conda shell.bash hook)"
-conda activate bpnp
-which python
+# eval "$(conda shell.bash hook)"
+# conda activate bpnp
+# which python
+source ~/.bpnp/bin/activate
 
 ########################### install deps env ###################################
-conda install -y -c conda-forge ipython ninja cython matplotlib opencv=3.4 tqdm requests six scipy
-conda install -y -c pytorch     pytorch torchvision cudatoolkit=10.1
+# conda install -y -c conda-forge ipython ninja cython matplotlib opencv=3.4 tqdm requests six scipy
+# conda install -y -c pytorch     pytorch torchvision cudatoolkit=10.1
 pip install kornia
+pip install -u ninja cython matplotlib opencv=3.4 tqdm requests six scipy
+pip install -u pytorch torchvision cudatoolkit=10.1
